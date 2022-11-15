4eedda3c528eba7cdcf4b798d8bac787c8174acb
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-15 17:37:04 +0000

    fixed with all the edge cases

--- sum/sum.js
+++ sum/sum.js
@@ -1,5 +1,19 @@
-let sum = 0;
-for (let i = 2; i < process.argv.length; i++) {
-  sum += Number(process.argv[i]);
+const sum = function(input) {
+  total = 0
+    for (let i = 2; i < input.length; i++) {
+      if (isNaN(Number(process.argv[i]))) {
+        console.log("Error: Only input numbers.");
+        process.exit();
+      }
+      if (process.argv.length < 4) {
+        console.log("Error: Must input at least 2 numbers.");
+        process.exit();
+      }
+      if (Number.isInteger(Number(process.argv[i]))) {
+        total += Number(process.argv[i]);
+      }
+    }
+  console.log(total);
 }
-console.log(sum);
+
+sum(process.argv);
